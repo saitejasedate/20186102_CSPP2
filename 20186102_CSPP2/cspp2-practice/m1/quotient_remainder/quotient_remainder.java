b222199e5f9c770cf788deb2f8aaf6c178f59324
saitejasedate <saitejavelagandula@msitprogram.net>
2018-08-27 19:18:45 +0530

    quotient_remainder program

---
+++ 20186102_CSPP2/cspp2-practice/m1/quotient_remainder/quotient_remainder.java
@@ -0,0 +1,12 @@
+public class quotient_remainder{
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 5;
+		int quotient;
+		int remainder;
+		quotient = a/b;
+		remainder = a%b;
+		System.out.println(quotient);
+		System.out.println(remainder);
+	}
+}
