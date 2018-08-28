845d288fc5564abf314306148635fe71831955d3
saitejasedate <saitejavelagandula@msitprogram.net>
2018-08-28 19:18:09 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- 20186102_CSPP2/cspp2-assignments/m2/Assignment-3/Assignment-3/Solution.java
+++ 20186102_CSPP2/cspp2-assignments/m2/Assignment-3/Assignment-3/Solution.java
@@ -3,46 +3,46 @@ import java.util.Scanner;
  * Class for solution.
  */
 public final class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
-		/**
-		 * { item_description }
-		 */
-	}
-	/**
-	 * { function_description }
-	 *
-	 * @param      base      The base
-	 * @param      exponent  The exponent
-	 *
-	 * @return     { description_of_the_return_value }
-	 */
-	public static long power(final int base, final int exponent) {
-		if (exponent > 0) {
-			return base * power(base, exponent - 1);
-		} else {
-			return 1;
-		}
-	
-	}
-	/**
-	 * It defines the main function.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(String[] args) {
-		/**
-		 * { var_description }
-		 */
-		Scanner s=new Scanner(System.in);      
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        /**
+         * { item_description }
+         */
+    }
+    /**
+     * { function_description }
+     *
+     * @param      base      The base
+     * @param      exponent  The exponent
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public static long power(final int base, final int exponent) {
+        if (exponent > 0) {
+            return base * power(base, exponent - 1);
+        } else {
+            return 1;
+        }
+    
+    }
+    /**
+     * It defines the main function.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        /**
+         * { var_description }
+         */
+        Scanner s = new Scanner(System.in);      
         int base = s.nextInt();
         int exponent = s.nextInt();
-        long result=power(base,exponent);
+        long result = power(base, exponent);
         System.out.println(result);
-	}
-	/*
-	Need to write the power function and print the output.
-	*/
+    }
+    /*
+    Need to write the power function and print the output.
+    */
 }
