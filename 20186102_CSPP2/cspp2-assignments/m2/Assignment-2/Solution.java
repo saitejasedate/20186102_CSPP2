0ce7efbd142e1ced3d8af0fda872406c91915022
saitejasedate <saitejavelagandula@msitprogram.net>
2018-08-28 18:44:04 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- 20186102_CSPP2/cspp2-assignments/m2/Assignment-2/Solution.java
+++ 20186102_CSPP2/cspp2-assignments/m2/Assignment-2/Solution.java
@@ -1,23 +1,35 @@
 import java.util.Scanner;
-
-public class Solution {
+/**
+ * { item_description }
+ */
+public final class Solution {
 	/*
 	Do not modify this main function.
+	
+	@param      args  The arguments
 	*/
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int a = scan.nextInt();
 		int b = scan.nextInt();
 		int c = scan.nextInt();
-		rootsOfQuadraticEquation(a, b, c);
+		roots(a, b, c);
 	}
 	/*
 	Need to write the rootsOfQuadraticEquation function and print the output.
 	*/
-	public static void rootsOfQuadraticEquation(int a,int b,int c){
-		long d = (b * b) - (4 * a *c);
+	
+	/**
+	 * { function_description }
+	 *
+	 * @param      a     { parameter_description }
+	 * @param      b     { parameter_description }
+	 * @param      c     { parameter_description }
+	 */
+	public static void roots(final int a, final int b, final int c){
+		long d = (b * b) - (2 * 2 * a *c);
 		double root1 = (-b + (Math.sqrt(Math.abs(d)))) / (2 * a);
 		double root2 = (-b - (Math.sqrt(Math.abs(d)))) / (2 * a);
-		System.out.println(root1 +" "+ root2);
+		System.out.println(root1 + " " + root2);
 	}
 }
