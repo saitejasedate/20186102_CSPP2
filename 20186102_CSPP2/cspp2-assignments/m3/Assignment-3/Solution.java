1c1edd8ef015ab22084114ae73ab61d080b06c31
saitejasedate <saitejavelagandula@msitprogram.net>
2018-08-29 17:35:19 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- 20186102_CSPP2/cspp2-assignments/m3/Assignment-3/Solution.java
+++ 20186102_CSPP2/cspp2-assignments/m3/Assignment-3/Solution.java
@@ -9,7 +9,7 @@ public class Solution {
      * @param      args  The arguments
      */
 
-    public static void main(String[] args) {
+    public static void main( final String[] args) {
 
         Scanner s = new Scanner(System.in);
         int n1 = s.nextInt();
