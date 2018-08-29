96f1c8a39dd2bd77074ad472d6e856702329207e
saitejasedate <saitejavelagandula@msitprogram.net>
2018-08-29 17:43:21 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- 20186102_CSPP2/cspp2-assignments/m3/Assignment-3/Solution.java
+++ 20186102_CSPP2/cspp2-assignments/m3/Assignment-3/Solution.java
@@ -11,7 +11,7 @@ public class Solution {
 
     public static void main(final String[] args) {
         /**.
-         * main function to print output. 
+         * main function to print output.
          */
         Scanner s = new Scanner(System.in);
         int n1 = s.nextInt();
