a53eb417527b59296c6b1547251e72f9b1e4e0e8
saitejasedate <saitejavelagandula@msitprogram.net>
2018-08-29 11:50:10 +0530

    SumOfNNumbers

---
+++ 20186102_CSPP2/cspp2-practice/m3/SumOfNNumbers/SumOfNNumbers.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class SumOfNNumbers{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int i = 1;
+		int sum = 0;
+		for(i = 1; i <= n; i++){
+			sum = sum + i;
+		}System.out.println(sum);
+	}
+}
