8fa289f167bc21a63db52b75830cd995e313fdeb
saitejasedate <saitejavelagandula@msitprogram.net>
2018-08-30 23:08:45 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

---
+++ 20186102_CSPP2/cspp2-assignments/m4/Assignment-1/Assignment-1/Solution.java
@@ -0,0 +1,29 @@
+
+import java.util.*;
+public class Solution
+{/*
+	Fill this main function to print maximum of given array
+	*/
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the size of an array:");
+		int array_size = sc.nextInt();
+		int[] array = new int[array_size];
+		System.out.println("Enter elements of an array:");
+		for(int i = 0; i < array_size; i++)
+		{
+			array[i] = sc.nextInt();
+		}
+		int max = array[0];
+		for(int i = 0; i < array_size; i++)
+		{
+			if(max < array[i])
+			{
+				max = array[i];
+			}
+		}
+		System.out.println("Maximum of elements in an array is:"+ max);
+
+	}
+}
