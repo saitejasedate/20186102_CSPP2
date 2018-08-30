091956739d6cfd0536060570283625e3834a4d9c
saitejasedate <saitejavelagandula@msitprogram.net>
2018-08-30 23:20:38 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- 20186102_CSPP2/cspp2-assignments/m4/Assignment-1/Assignment-1/Solution.java
+++ 20186102_CSPP2/cspp2-assignments/m4/Assignment-1/Assignment-1/Solution.java
@@ -1,24 +1,24 @@
-
 import java.util.*;
-public class Solution {/*
-	Fill this main function to print maximum of given array
-	*/
-	public static void main(final String[] args) {
-		Scanner sc = new Scanner(System.in);
-		// System.out.println("Enter the size of an array:");
-		int arraysize = sc.nextInt();
-		int[] array = new int[arraysize];
-		// System.out.println("Enter elements of an array:");
-		for (int i = 0; i < arraysize; i++) {
-			array[i] = sc.nextInt();
-		}
-		int max = array[0];
-		for (int i = 0; i < arraysize; i++) {
-			if (max < array[i]) {
-				max = array[i];
-			}
-		}
-		System.out.println(max);
+public class Solution {
+    /*
+    Fill this main function to print maximum of given array
+    */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter the size of an array:");
+        int arraysize = sc.nextInt();
+        int[] array = new int[arraysize];
+        // System.out.println("Enter elements of an array:");
+        for (int i = 0; i < arraysize; i++) {
+            array[i] = sc.nextInt();
+        }
+        int max = array[0];
+        for (int i = 0; i < arraysize; i++) {
+            if (max < array[i]) {
+                max = array[i];
+            }
+        }
+        System.out.println(max);
 
-	}
+    }
 }
