60f8b9646c846320fa9b11eefa5a3494dcf9578b
saitejasedate <saitejavelagandula@msitprogram.net>
2018-08-31 17:26:04 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- 20186102_CSPP2/cspp2-assignments/m4/Assignment-4/Assignment-4/Solution.java
+++ 20186102_CSPP2/cspp2-assignments/m4/Assignment-4/Assignment-4/Solution.java
@@ -1,21 +1,21 @@
 import java.util.Scanner;
-public class Solution
+public final class Solution
 {/*
-	Do not modify the main function 
-	*/
-	public static void main(String[] args)
-	{
-		Scanner sc = new Scanner(System.in);
-		String str = sc.nextLine();
-		String reverse = reverseString(str);	
-		System.out.println(reverse);
-		
-	}
-	//Write reverseString function
-	public static String reverseString(String str) {
-		String output = "";
-		for(int element = str.length() - 1; element >= 0; element--) {
-			output  = output + str.charAt(element);
-		}return output; 
-	}
+    Do not modify the main function 
+    */
+    public static void main(final String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        String reverse = reverseString(str);    
+        System.out.println(reverse);
+        
+    }
+    //Write reverseString function
+    public static String reverseString(final String str) {
+        String output = "";
+        for(int element = str.length() - 1; element >= 0; element--) {
+            output  = output + str.charAt(element);
+        }return output;
+    }
 }
