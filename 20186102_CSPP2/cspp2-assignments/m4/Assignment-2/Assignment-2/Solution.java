c55927c3246f733936b7d1d302731bb342b1916f
saitejasedate <saitejavelagandula@msitprogram.net>
2018-08-31 21:04:44 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- 20186102_CSPP2/cspp2-assignments/m4/Assignment-2/Assignment-2/Solution.java
+++ 20186102_CSPP2/cspp2-assignments/m4/Assignment-2/Assignment-2/Solution.java
@@ -6,41 +6,30 @@ public class Solution {
         int row = sc.nextInt();
         int col = sc.nextInt();
         int[][] matrix1 = new int[row][col];
-        for(int i = 0; i < row; i++){
-            for(int j = 0; j<col; j++){
+        for(int i = 0; i < row; i++) {
+            for(int j = 0; j<col; j++) {
                 matrix1[i][j] = sc.nextInt();
             }
         }
         int row1 = sc.nextInt();
         int col1 = sc.nextInt();
         int[][] matrix2 = new int[row1][col1];
-        for(int i = 0; i < row1; i++){
-            for(int j = 0; j<col1; j++){
+        for(int i = 0; i < row1; i++) {
+            for(int j = 0; j<col1; j++) {
                 matrix2[i][j] = sc.nextInt();
             }
         }
         print(matrix1, matrix2);
     }
-    /*public static void print(int[][] matrix){
-        for(int i = 0; i < matrix.length; i++){
-            String s = "";
-            for(int j = 0; j<matrix[0].length; j++){
-                s += matrix[i][j] +" ";
-            }
-            
-            System.out.println(s.trim());
-        }
-    }*/
-    static void print(int[][] matrix1, int[][] matrix2){
+    static void print(int[][] matrix1, int[][] matrix2) {
         int x = matrix1.length, y = matrix1[0].length;
         int u = matrix2.length, v = matrix2[0].length;
-        if (x == u & y == v){
-            for(int i = 0; i < matrix1.length; i++){
+        if (x == u & y == v) {
+            for (int i = 0; i < matrix1.length; i++) { 
                 String s = "";
-                for(int j = 0; j<matrix1[0].length; j++){
-                    s += matrix1[i][j]+matrix2[i][j] +" ";
-                }
-                System.out.println(s.trim());
+                for (int j = 0; j<matrix1[0].length; j++) {
+                    s += matrix1[i][j] + matrix2[i][j] +" ";
+                } System.out.println(s.trim());
             } 
         } else {
                 System.out.println("not possible");
@@ -49,14 +38,4 @@ public class Solution {
 }
     
 
-    // public static void main(String[] args) {
-    //  int[] arr = {2, 2, 5, 6, 7, 7};
-    //  for(int i = 0; i<arr.length-1;i++){
-    //      for(int j = i+1; j <arr.length;j++){
-    //          if(arr[i] == arr[j]){
-    //              System.out.println(arr[i]);
-    //          }
-    //      }
-    //  }
     
-// }
