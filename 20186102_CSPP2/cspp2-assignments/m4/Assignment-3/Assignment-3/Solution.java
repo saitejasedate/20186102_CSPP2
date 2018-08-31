c192ac157158d93eb2396c65a8d0cd4d84df82da
saitejasedate <saitejavelagandula@msitprogram.net>
2018-08-31 19:34:00 +0530

    "check_style_score -> 1 of 1 passed. style score: 0/1 "

--- 20186102_CSPP2/cspp2-assignments/m4/Assignment-3/Assignment-3/Solution.java
+++ 20186102_CSPP2/cspp2-assignments/m4/Assignment-3/Assignment-3/Solution.java
@@ -7,9 +7,9 @@ public class Solution {
     Do not modify this main function.
     */
     public static void main(final String[] args) {
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         int numberofinputs = sc.nextInt();
-        for(int i = 0; i < numberofinputs; i++) {
+        for (int i = 0; i < numberofinputs; i++) {
             String s = sc.next();
             String res = binaryToDecimal(s);
             System.out.println(res);
@@ -18,8 +18,8 @@ public class Solution {
     static String binaryToDecimal(final String output) {
         String str = "";
         int sum = 0;
-        for(int i = output.length()-1; i >= 0; i--) {
-            if(output.charAt(i) == '1') {
+        for (int i = output.length()-1; i >= 0; i--) {
+            if (output.charAt(i) == '1') {
                 sum = sum + (int) Math.pow(2, output.length() - i - 1);
             }
         }
