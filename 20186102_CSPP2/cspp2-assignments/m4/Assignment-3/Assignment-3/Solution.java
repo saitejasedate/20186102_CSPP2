830f07dbd3c8d359fbe645be98df64ac1def528e
saitejasedate <saitejavelagandula@msitprogram.net>
2018-08-31 19:32:11 +0530

    "check_style_score -> 1 of 1 passed. style score: 0/1 "

--- 20186102_CSPP2/cspp2-assignments/m4/Assignment-3/Assignment-3/Solution.java
+++ 20186102_CSPP2/cspp2-assignments/m4/Assignment-3/Assignment-3/Solution.java
@@ -4,24 +4,23 @@
 import java.util.Scanner;
 public class Solution
 {/*
-	Do not modify this main function.
-	*/
-	public static void main(String[] args)
-	{
-		Scanner sc=new Scanner(System.in);
-		int numberofinputs = sc.nextInt();
-		for(int i = 0;i < numberofinputs;i++){
-			String s = sc.next();
-			String res = binaryToDecimal(s);
-			System.out.println(res);
-		}
-	}
-	static String binaryToDecimal(String output) {
+    Do not modify this main function.
+    */
+    public static void main(final String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int numberofinputs = sc.nextInt();
+        for(int i = 0;i < numberofinputs;i++) {
+            String s = sc.next();
+            String res = binaryToDecimal(s);
+            System.out.println(res);
+        }
+    }
+    static String binaryToDecimal(final String output) {
         String str = "";
         int sum = 0;
         for(int i = output.length()-1; i >= 0; i--) {
             if(output.charAt(i) == '1'){
-                sum = sum +(int) Math.pow(2,output.length() - i - 1);
+                sum = sum + (int) Math.pow(2, output.length() - i - 1);
             }
         }
         str += sum;
