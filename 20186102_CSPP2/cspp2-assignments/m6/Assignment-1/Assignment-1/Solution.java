1866739c2a80dee7879310882b2711732941a64b
saitejasedate <saitejavelagandula@msitprogram.net>
2018-09-01 12:14:45 +0530

    "check_style_score -> 6 of 6 passed. style score: 0/1 "

--- 20186102_CSPP2/cspp2-assignments/m6/Assignment-1/Assignment-1/Solution.java
+++ 20186102_CSPP2/cspp2-assignments/m6/Assignment-1/Assignment-1/Solution.java
@@ -19,9 +19,9 @@ public final class Solution {
      */
     static void oddComposites(final int n) {
         final int nine = 9;
-        for (int number = 9; number <= n ; number++) {
+        for (int number = nine; number <= n ; number++) {
             int count = 1;
-            for(int i = 2; i < number / 2; i++) {
+            for (int i = 2; i < number / 2; i++) {
                 if (number % i == 0) {
                     count = 0;
                     break;
