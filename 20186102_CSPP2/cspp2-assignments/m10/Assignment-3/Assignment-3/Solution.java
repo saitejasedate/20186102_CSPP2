b76bf95f47ff11e36074f4931bdc189e4ae9044b
saitejasedate <saitejavelagandula@msitprogram.net>
2018-09-06 16:40:10 +0530

    "check_style_score -> 6 of 6 passed. style score: 0/1 "

--- 20186102_CSPP2/cspp2-assignments/m10/Assignment-3/Assignment-3/Solution.java
+++ 20186102_CSPP2/cspp2-assignments/m10/Assignment-3/Assignment-3/Solution.java
@@ -326,13 +326,16 @@ public class Solution {
 		                listStudent.remove(Integer.parseInt(tokens[1]));
 		                break;
 		                case "indexOf":
-		                System.out.println(listStudent.indexOf(new Student(tokens[1])));
+		                System.out.println(listStudent.indexOf
+		                	(new Student(tokens[1])));
 		                break;
 		                case "get":
-		                System.out.println(listStudent.get(Integer.parseInt(tokens[1])));
+		                System.out.println(listStudent.get
+		                	(Integer.parseInt(tokens[1])));
 		                break;
 		                case "contains":
-		                System.out.println(listStudent.contains(new Student(tokens[1])));
+		                System.out.println(listStudent.contains
+		                	(new Student(tokens[1])));
 		                break;
 		            }
 		        }
