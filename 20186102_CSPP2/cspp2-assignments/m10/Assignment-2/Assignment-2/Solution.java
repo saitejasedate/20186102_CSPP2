9564b6c699a13bbeb10223ca2d4a9ec1ac539858
saitejasedate <saitejavelagandula@msitprogram.net>
2018-09-06 14:25:09 +0530

    "check_style_score -> 3 of 3 passed. style score: 0/1 "

--- 20186102_CSPP2/cspp2-assignments/m10/Assignment-2/Assignment-2/Solution.java
+++ 20186102_CSPP2/cspp2-assignments/m10/Assignment-2/Assignment-2/Solution.java
@@ -52,6 +52,7 @@ public class Solution {
                 case "contains":
                 System.out.println(l.contains(tokens[1]));
                 break;
+                default:
             }
         }
 	}
